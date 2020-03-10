0bbac42ffe7b04a877bcf85a64d26280d82faa9f
arnonphumkaow <62021707+arnonphumkaow@users.noreply.github.com>
2020-03-10 21:27:02 +0700

    Add files via upload

---
+++ a.php
@@ -0,0 +1,14 @@
+<?php
+
+// $response = file_get_contents("http://61.19.255.12:9638/api/tnvdev/v1/ids?id=734");
+
+// $ID = $response->ID;
+
+// echo $ID;
+echo 123;
+
+// $index = '{ "index": { "_id": "ID" } }';
+
+// echo $response;
+
+?>
